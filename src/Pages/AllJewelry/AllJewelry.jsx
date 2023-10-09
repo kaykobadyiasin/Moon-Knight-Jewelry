e824277a1d35db8097154f70e1c4282cc7de7d9f
Kaykobad Ahammed Yiasin <kaykobadyiasin@gmail.com>
2023-10-09 23:08:36 +0600

    showing firebase deploing

--- src/Pages/AllJewelry/AllJewelry.jsx
+++ src/Pages/AllJewelry/AllJewelry.jsx
@@ -2,7 +2,6 @@ import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 const AllJewelry = () => {
-
     const [jewelrys, setJewelrys] = useState();
 
     useEffect(() => {
